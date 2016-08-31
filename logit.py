c977a18420df107f6ca3e4d0f3e829724ec6b812
Brightcells <brightcells@gmail.com>
2016-08-31 14:43:13 +0800

    default not log body and res & log get or post

--- logit.py
+++ logit.py
@@ -11,25 +11,47 @@ def loggerit(func, flag, content):
     logger.debug('func=%s&flag=%s&content=%s', func, flag, content)
 
 
-def logit(func):
-    @wraps(func)
-    def with_logging(request, *args, **kwargs):
-        # 获取函数名
-        name = func.__name__
-        # 打印 body 内容到日志文件
-        try:
-            loggerit(name, 'body', request.body)
-        except Exception as e:
-            loggerit(name, 'error', e.message)
-        # 打印 get 内容到日志文件
-        loggerit(name, 'get', request.GET)
-        # 打印 POST 内容到日志文件
-        loggerit(name, 'post', request.POST)
-        res = func(request, *args, **kwargs)
-        # 打印 response 内容到日志文件
-        try:
-            loggerit(name, 'res', res.content)
-        except Exception as e:
-            loggerit(name, 'error', e.message)
-        return res
-    return with_logging
+def logit(func=None, body=False, res=False):
+    def decorator(func):
+        @wraps(func)
+        def with_logging(request, *args, **kwargs):
+            # Get Func Name
+            name = func.__name__
+
+            # Body Content Section
+            if body:
+                try:
+                    loggerit(name, 'body', request.body)
+                except Exception as e:
+                    loggerit(name, 'error', e.message)
+
+            # Get Content Section
+            rgets = request.GET
+            if rgets:
+                loggerit(name, 'get', request.GET)
+
+            # POST Content Section
+            rposts = request.POST
+            if rposts:
+                loggerit(name, 'post', request.POST)
+
+            response = func(request, *args, **kwargs)
+
+            # Response Content Section
+            if res:
+                try:
+                    loggerit(name, 'res', response.content)
+                except Exception as e:
+                    loggerit(name, 'error', e.message)
+
+            return response
+
+        return with_logging
+
+    if not func:
+        def decorator2(func):
+            return decorator(func)
+
+        return decorator2
+
+    return decorator(func)
