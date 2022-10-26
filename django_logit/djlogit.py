97f2be0586188422cfff0b569f18d4569389ce07
huangqimin001 <huangqimin001@ke.com>
2022-10-26 11:43:35 +0800

    :art: Support logit FILES

--- django_logit/djlogit.py
+++ django_logit/djlogit.py
@@ -23,30 +23,35 @@ def logit(func=None, body=False, res=False, session=False, cookie=False):
             if hasattr(settings, 'DJANGO_LOGIT_ENABLED') and settings.DJANGO_LOGIT_ENABLED:
 
                 # Body Content Section
-                if body:
+                if body or (hasattr(settings, 'DJANGO_LOGIT_ENABLED_BODY') and settings.DJANGO_LOGIT_ENABLED_BODY):
                     try:
                         loggerit(name, 'body', request.body)
                     except Exception as e:
                         loggerit(name, 'error', e.message)
 
-                # Get Content Section
+                # GET Content Section
                 rgets = request.GET
                 if rgets:
-                    loggerit(name, 'get', request.GET)
+                    loggerit(name, 'get', rgets)
 
                 # POST Content Section
                 rposts = request.POST
                 if rposts:
-                    loggerit(name, 'post', request.POST)
+                    loggerit(name, 'post', rposts)
 
-                # Session Content Section
-                if session:
+                # FILES Content Section
+                rfiles = request.FILES
+                if rfiles:
+                    loggerit(name, 'file', rfiles)
+
+                # SESSION Content Section
+                if session or (hasattr(settings, 'DJANGO_LOGIT_ENABLED_SESSION') and settings.DJANGO_LOGIT_ENABLED_SESSION):
                     session_items = request.session.items()
                     if session_items:
                         loggerit(name, 'session', session_items)
 
-                # Cookie Content Section
-                if cookie:
+                # COOKIES Content Section
+                if cookie or (hasattr(settings, 'DJANGO_LOGIT_ENABLED_COOKIE') and settings.DJANGO_LOGIT_ENABLED_COOKIE):
                     cookies = request.COOKIES
                     if cookies:
                         loggerit(name, 'cookie', cookies)
@@ -54,7 +59,7 @@ def logit(func=None, body=False, res=False, session=False, cookie=False):
                 response = func(request, *args, **kwargs)
 
                 # Response Content Section
-                if res:
+                if res or (hasattr(settings, 'DJANGO_LOGIT_ENABLED_RES') and settings.DJANGO_LOGIT_ENABLED_RES):
                     try:
                         loggerit(name, 'res', response.content)
                     except Exception as e:
